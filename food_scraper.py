b469b456ef5ffbb635aab1d65f7f736a79134bbb
LeRoi <kxhere@gmail.com>
2017-09-16 14:55:46 -0400

    Continue food scraper work
    Scraper now takes day offset argument.
    Scraper now filters to only events with food.
    Clean up event description formatting a bit.

--- food_scraper.py
+++ food_scraper.py
@@ -3,22 +3,43 @@
 
 import datetime, json, sys, urllib
 
-food_terms = ["lunch", "dinner", "snack", "food", "served"]
-non_food_terms = ["no"]
+food_terms = ["lunch", "dinner", "snack", "food", "served", "provided"]
 
 day_url = "http://hls.harvard.edu/calendar/%s"
 event_marker = 'class="url"'
 description_marker = 'type="application/ld+json"'
 
+time_format = "%Y-%m-%dT%H:%M:%S+00:00"
+json_parse_error = "Error parsing json from %s"
+
 class Event:
-    def __init__(self, name, start, end, food):
+    def __init__(self, name, start, end, description, error=None):
         self.name = name
         self.start = start
         self.end = end
-        self.food = food
+        self.food = self.get_food_sentences(description)
+        self.error = error
+
+    def has_food(self):
+        return not self.error and self.food
+
+    def get_food_sentences(self, text):
+        if not text:
+            return None
+        food = ''
+        sentences = text.split(".")
+        for sentence in sentences:
+            score = 0
+            for word in food_terms:
+                score += sentence.count(word)
+            if score > 0:
+                food += " %s." % sentence.strip()
+        return food if len(food) > 0 else None
 
     def __str__(self):
-        return "%s: (%s - %s), %s" % \
+        if self.error:
+            return self.error
+        return "%s: (%s - %s). %s" % \
                (self.name, str(self.start), str(self.end), self.food)
 
 def extract_url(line):
@@ -36,13 +57,25 @@ def get_events(date):
     return urls
 
 def get_event(url):
-    event = get_filtered_lines(url, description_marker)[0]
-    details = json.loads(event.split("[")[1].split("]")[0])
-    return details
+    event_data = get_filtered_lines(url, description_marker)[0]
+    try:
+        details = json.loads(event_data.split("[")[1].split("]")[0])
+        start = datetime.datetime.strptime(details['startDate'], time_format)
+        end = datetime.datetime.strptime(details['endDate'], time_format)
+        event = Event(details['name'].encode('utf8'), start, end, details['description'].encode('utf8'))
+    except ValueError:
+        event = Event(None, None, None, None, json_parse_error % url)
+    return event
 
 def main():
-    for line in get_events(datetime.datetime.today().date()):
-        print get_event(line)['description'].encode('utf8')
+    date_offset = 0 if len(sys.argv) < 2 else int(sys.argv[1])
+    today = datetime.datetime.today()
+    offset = today + datetime.timedelta(days = date_offset)
+    events = [get_event(event) for event in get_events(offset.date())]
+    for event in events:
+        if not event.has_food():
+            continue
+        print "%s\n" % event
 
 if __name__ == "__main__":
     main()
