a4fc72e7755651d20e110f5e90dbd8bd1d8315ff
LeRoi <kxhere@gmail.com>
2017-09-18 03:42:04 -0400

    Add flask server for food_scraper

---
+++ flask_food_server.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import datetime, food_scraper
+
+day_cache = {}
+event_cache = {}
+
+app = Flask(__name__)
+
+@app.route('/')
+def get_lunches():
+	global day_cache, event_cache
+	today = datetime.datetime.today()
+	today = today + datetime.timedelta(days = 0)
+	food = food_scaper.get_food_listings(today.date, day_cache, event_cache)
+	return "hi"
