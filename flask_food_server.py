460daf5eb205bac2947a6322f3f205cf255e52ba
LeRoi <kxhere@gmail.com>
2017-09-18 04:21:00 -0400

    Try to fix favicon error

--- flask_food_server.py
+++ flask_food_server.py
@@ -1,18 +1,23 @@
 from flask import Flask
-import datetime, food_scraper, os
+#import datetime, food_scraper, os
 
-day_cache = {}
-event_cache = {}
+#day_cache = {}
+#event_cache = {}
 
 app = Flask(__name__)
 
 @app.route('/')
 def get_lunches():
-	global day_cache, event_cache
-	today = datetime.datetime.today()
-	today = today + datetime.timedelta(days = 0)
-	food = food_scaper.get_food_listings(today.date, day_cache, event_cache)
+	#global day_cache, event_cache
+	#today = datetime.datetime.today()
+	#today = today + datetime.timedelta(days = 0)
+	#food = food_scaper.get_food_listings(today.date, day_cache, event_cache)
 	return "hi"
+    
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static', 'images'),
+                               'favicon.ico', mimetype='image/png')
     
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
