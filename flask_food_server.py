c6802985f0f3039a5985add76afaadea8f18208c
LeRoi <kxhere@gmail.com>
2017-09-18 04:23:40 -0400

    Try adding some behavior back to server

--- flask_food_server.py
+++ flask_food_server.py
@@ -1,16 +1,15 @@
 from flask import Flask
-import os
-#import datetime, food_scraper, os
+import datetime, food_scraper, os
 
-#day_cache = {}
-#event_cache = {}
+day_cache = {}
+event_cache = {}
 
 app = Flask(__name__)
 
 @app.route('/')
 def get_lunches():
 	#global day_cache, event_cache
-	#today = datetime.datetime.today()
+	today = datetime.datetime.today()
 	#today = today + datetime.timedelta(days = 0)
 	#food = food_scaper.get_food_listings(today.date, day_cache, event_cache)
 	return "hi"
