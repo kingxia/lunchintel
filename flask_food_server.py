7523a4a217767399230289637682f6c1dad70c99
LeRoi <kxhere@gmail.com>
2017-09-18 04:24:52 -0400

    Restore food functionality; fix typo

--- flask_food_server.py
+++ flask_food_server.py
@@ -8,10 +8,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def get_lunches():
-	#global day_cache, event_cache
+	global day_cache, event_cache
 	today = datetime.datetime.today()
-	#today = today + datetime.timedelta(days = 0)
-	#food = food_scaper.get_food_listings(today.date, day_cache, event_cache)
+	today = today + datetime.timedelta(days = 0)
+	food = food_scaper.get_food_listings(today.date(), day_cache, event_cache)
 	return "hi"
     
 @app.route('/favicon.ico')
