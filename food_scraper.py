942e82d80aca64ad52dd206b7c4008d414a78b46
LeRoi <kxhere@gmail.com>
2017-09-18 12:45:02 -0400

    Grab entire description

--- food_scraper.py
+++ food_scraper.py
@@ -82,6 +82,7 @@ def get_event(url, event_cache={}):
     page_data = [line for line in url_lines.iter_lines()]
     event_data = [line for line in page_data if description_marker in line][0]
     index = [i for i, s in enumerate(page_data) if "Event content" in s][0]
+    index_2 = [i for i, s in enumerate(page_data) if "tribe-events-single-event-description" in s][0]
     try:
         details = json.loads(event_data.split("[")[1].split("]")[0])
         #details = page_data[index + 2].strip()
@@ -89,7 +90,11 @@ def get_event(url, event_cache={}):
         #details = [3:len(details)-4]
         start = datetime.datetime.strptime(details['startDate'], time_format)
         end = datetime.datetime.strptime(details['endDate'], time_format)
-        description = page_data[index + 2].strip()
+        description = ''
+        for i in range(index, index_2):
+            description += page_data[i].strip()
+        #description = page_data[index + 2].strip()
+        #print 'Description: %s' % description
         #description = [3:len(details-4)]
         event = Event(details['name'].encode('utf8'), start, end, description)
         #event = Event(details['name'].encode('utf8'), start, end, details['description'].encode('utf8'))
