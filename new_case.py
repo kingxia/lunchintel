a541628518d6f5962722343a0c70853ea69b1c44
LeRoi <kxhere@gmail.com>
2017-09-25 00:42:00 -0400

    new_case.py now opens document after creating

--- new_case.py
+++ new_case.py
@@ -1,4 +1,4 @@
-import sys
+import os, sys, time
 from shutil import copyfile
 
 year = "1L"
@@ -21,8 +21,9 @@ def main():
                else sys.argv[1]
     classname = get_class_name() if len(sys.argv) < 3 \
                 else classmap[sys.argv[2].lower()]
-    copyfile("../Brief Template.docx", \
-             "../%s/%s/%s/Briefs/%s.docx" % (year, term, classname, casename))
+    target = "../%s/%s/%s/Briefs/%s.docx" % (year, term, classname, casename)
+    copyfile("../Brief Template.docx", target)
+    os.startfile(os.path.join(os.path.dirname(os.path.abspath(__file__)), target))
     
 if __name__ == "__main__":
     main()
